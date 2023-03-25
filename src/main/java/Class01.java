7791a91bd0376680e9f383da506fd7c59961d191
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-03-25 11:51:42 +0100

    v4

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -3,6 +3,8 @@ public class Class01 {
 
         System.out.println("birinci calisma");
         System.out.println("v3");
+        System.out.println("yeni deneme");
+        System.out.println("yeni degisiklik");
 
 
 
